4a50e4a77374338f56e33a8fbd5e03f964ca42d1
GwydionBr <gwydie@googlemail.com>
2025-01-06 22:07:36 +0100

    Initial Commit

---
+++ Time Tracker/Time_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_TrackerApp.swift
+//  Time Tracker
+//
+//  Created by Gwydion Braunsdorf on 06.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Time_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
