5eff5b600bfa79c8a4510a472c11e5bb72812a4a
GwydionBr <gwydie@googlemail.com>
2025-01-10 17:03:41 +0100

    Refactor Projects Detail to Show a horizontal SplitView with the SessionList and the TimeTrackerView.

--- Time Tracker/Views/Project/ProjectDetail.swift
+++ Time Tracker/Views/Project/ProjectDetail.swift
@@ -8,47 +8,31 @@
 import SwiftUI
 
 struct ProjectDetail: View {
-    
     @ObservedObject var timerData: TimerData
     @Binding var project: TimerProject
-    
-    @State private var listWidth: CGFloat = 200
-    
+
     var body: some View {
-        GeometryReader { geometry in
-                    HStack(spacing: 0) {
-                        SessionList(timerData: TimerData(), project: $project)
-                        .background(Color.gray.opacity(0.2))
-                        .frame(width: listWidth)
+        HSplitView {
+            // Linke Ansicht
+            SessionList(timerData: TimerData(), project: $project)
+                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: .infinity) // Flexible Breite
+                .background(Color.gray.opacity(0.2))
+                .layoutPriority(1) // Priorität erhöhen
 
-                        Divider()
-                            .frame(width: 3)
-                            .background(Color.gray)
-                            .gesture(
-                                DragGesture()
-                                    .onChanged { value in
-                                        let newWidth = listWidth + value.translation.width
-                                        if newWidth > 100 && newWidth < geometry.size.width - 100 {
-                                            listWidth = newWidth
-                                        }
-                                    }
-                            )
+            // Rechte Ansicht
+            TimeTrackerView(project: $project)
+                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: .infinity) // Flexible Breite
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .navigationTitle("\(project.title) - \(project.description)")
+    }
+}
 
-                        VStack {
-                            TimeTrackerView(project: $project)
-                                
-                        }
-                        .frame(width: geometry.size.width - listWidth - 5)
-                    }
-                }
 //        HStack {
 //            SessionList(timerData: TimerData(), project: $project)
 //            TimeTrackerView()
 //                .padding()
 //        }
-//        .navigationTitle("\(project.title) - \(project.description)")
-    }
-}
 
 #Preview {
     ProjectDetail(timerData: TimerData(), project: .constant(TimerData().projects[0]))
